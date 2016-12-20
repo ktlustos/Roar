3fad28d25b093780565c8795e052427028e64b5c
Kent <ktlustos@arrayschool.com>
2016-12-20 16:37:32 -0700

    full setup

--- src/JSX/NavBar.js
+++ src/JSX/NavBar.js
@@ -1,1 +1,16 @@
 import React from 'react';
+
+var NavBar = React.createClass({
+
+  render: function() {
+    return (
+    <ul>
+      <li>Home</li>
+      <li>Sign Up</li>
+      <li>Log In</li>
+    </ul>
+    )
+  }
+});
+
+module.exports = NavBar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
+// React Imports
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// JSX Components Imports
+import NavBar from './JSX/NavBar';
+
+// CSS Import
 import './Stylesheets/css/index.css';
 
 var MasterComponent = React.createClass({
 
   render: function() {
     return (
-      <div>Hello</div>
+      <NavBar />
     )
   }
 });
